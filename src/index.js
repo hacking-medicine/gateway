a3d7f5e56b5327a7970a86db8d9bb2ae8f2d2051
Erpheus <erpheus@gmail.com>
2015-02-28 14:13:35 +0100

    blablabla

--- src/index.js
+++ src/index.js
@@ -5,8 +5,22 @@ var cors = require('cors');
 var db = require('./db.js')
 var RT = require('./rt.js')
 
+dataType :: Int, value :: Double, time :: UTCTime, patient :: Int
+estos los mandas a /events/:id
+donde :id ≝ patient
+
 var rt_analysis = RT({host: 'localhost', port: '3000'});
-rt_analysis.start();
+rt_analysis.start(
+function(e){
+	return {
+		dataType: param_map[e.param_t],
+		value: e.value,
+		time: e.timestamp,
+		patient: e.user_id
+	}
+}
+
+	);
 
 var rt_notifier = RT({host: 'localhost', port: '3001'});
 rt_notifier.start();
@@ -33,8 +47,14 @@ db.get_params(function(map){
 
 
 app.post('/monitor', function (req, res) {
+	console.log('REQUEST TO MONITOR');
 	var user_id = req.body.user_id;
 	var measurement_list = req.body.measurements;
+
+	measurement_list = measurement_list.map(function(measurement){
+		measurement.user_id = user_id;
+		return measurement;
+	})
 
 	for (var i = measurement_list.length - 1; i >= 0; i--) {
 		db.save_measurement(user_id, measurement_list[i]);
